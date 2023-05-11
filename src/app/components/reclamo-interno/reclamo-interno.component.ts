6b6f8a4d40af977050d69f94a5be78152d2273db
FrancoGriffone <pipokn@gmail.com>
2023-05-11 10:18:46 -0300

    se dejo de usar el componente prueba, se paso todo al reclamo interno y prueba pasar a usarse para otros testeos

--- src/app/components/reclamo-interno/reclamo-interno.component.ts
+++ src/app/components/reclamo-interno/reclamo-interno.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reclamo-interno',
   templateUrl: './reclamo-interno.component.html',
-  styleUrls: ['./reclamo-interno.component.scss']
+  styleUrls: ['./reclamo-interno.component.scss'],
 })
 export class ReclamoInternoComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
+  profileForm = new FormGroup({
+    codigoBarras: new FormControl(''),
+    seccion: new FormControl(''),
+    estado: new FormControl(''),
+    motivo: new FormControl('', Validators.required),
+    observaciones: new FormControl(''),
+    taller: new FormControl(''),
+    importe: new FormControl(''),
+    pagado: new FormControl(''),
+  });
 
-  ngOnInit(): void {
+  onSubmit() {
+    // TODO: Use EventEmitter with form value
+    console.log(this.profileForm.value);
   }
-
 }
--- src/app/prueba/prueba.component.ts
+++ src/app/prueba/prueba.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-prueba',
@@ -10,18 +9,4 @@ export class PruebaComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {}
-
-  profileForm = new FormGroup({
-    codigoBarras: new FormControl(''),
-    seccion: new FormControl(''),
-    estado: new FormControl(''),
-    motivo: new FormControl(''),
-    observaciones: new FormControl(''),
-    taller: new FormControl(''),
-  });
-
-  onSubmit() {
-    // TODO: Use EventEmitter with form value
-    console.log(this.profileForm.value);
-  }
 }
