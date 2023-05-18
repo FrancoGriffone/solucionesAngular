b1f70dcc33990f35bc58ed4cd3d0ac38eadbac3d
FrancoGriffone <pipokn@gmail.com>
2023-05-18 09:57:30 -0300

    routing inicio, bcargo y reclamointerno ++ edit style de nabvar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { BCargoComponent } from './components/b-cargo/b-cargo.component';
+import { ReclamoInternoComponent } from './components/reclamo-interno/reclamo-interno.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: InicioComponent,
+  },
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'bcargo',
+    component: BCargoComponent,
+  },
+  {
+    path: 'reclamointerno',
+    component: ReclamoInternoComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
