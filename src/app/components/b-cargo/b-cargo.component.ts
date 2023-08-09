3b164e6b9f8fac25bddc0a5bb4d9267ab4bdcba9
Franco Griffone <pipokn@gmail.com>
2023-08-09 10:45:23 -0300

    bcargo ahora trae el reporte. ver posibilidad de que se plasme en la pantalla

--- src/app/components/b-cargo/b-cargo.component.ts
+++ src/app/components/b-cargo/b-cargo.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-b-cargo',
@@ -7,13 +8,48 @@ import { FormControl } from '@angular/forms';
   styleUrls: ['./b-cargo.component.scss'],
 })
 export class BCargoComponent implements OnInit {
-  constructor() {}
 
-  ngOnInit(): void {}
-  date = new FormControl(new Date());
+  //LINKS BOLETAS DE CARGO
+  bcargo1: string = "http://192.168.0.111/ReportServer/Pages/ReportViewer.aspx?%2fReclamos%2fInfRecABC_det&rs:Command%20=%20Render&IdEmp="
+  bcargo2: string = "&Fecha="
+  idEmp: string = ""
+  bcargoFecha: string = ""
 
+  // FECHA + FUNCION PARA RESTAR UN DIA + SETEAR DIA OBTENIDO
+  fecha = new Date()
+
+  subtractDay(date: Date, day: number) {
+    date.setDate(date.getDate() - day);
+    return date;
+  }
+
+  fechaBusqueda: any = this.subtractDay(this.fecha, 1).toISOString().slice(0,-14)
+
+  //FORM CONTROL SOBRE LA FECHA
+  date = new FormControl(this.fechaBusqueda);
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    let local: string = this.route.snapshot.paramMap.get('local') || ''
+    if (local == 'Tate') {
+      this.idEmp = 'T'
+    } else if (local == 'Kilroy') {
+      this.idEmp = 'K'
+    } else if (local == 'KilroyKids') {
+      this.idEmp = 'N'
+    } else if (local == 'TateExpress') {
+      this.idEmp = 'E'
+    } else if (local == 'KitExpress') {
+      this.idEmp = 'M'
+    } else {
+      this.idEmp = 'T'
+    }
+  }
+  
   onSubmit() {
-    // TODO: Use EventEmitter with form value
-    console.log(this.date.value);
+    this.bcargoFecha = this.date.value
+    let linkBCargo: string = this.bcargo1 + this.idEmp + this.bcargo2 + this.bcargoFecha
+    window.open(linkBCargo, '_blank')
   }
 }
