da3f142e70fe67b93e5a815fe04c19aca5364356
Franco Griffone <pipokn@gmail.com>
2023-09-12 08:50:59 -0300

    actualizado los reclamos internos, ya se pueden cargar y ver con los datos relacionados a los id

--- src/app/components/reclamo-interno/reclamo-interno.component.ts
+++ src/app/components/reclamo-interno/reclamo-interno.component.ts
@@ -16,6 +16,8 @@ export class ReclamoInternoComponent implements OnInit {
 
   talleres: any
 
+  talleresActivos: any[] = []
+
   opciones: any
 
   producto: any //PARA VOLCAR LA DESCRIPCION DEL PRODUCTO
@@ -44,7 +46,7 @@ export class ReclamoInternoComponent implements OnInit {
   });
 
   constructor(private api: ApiService,
-    private route: ActivatedRoute, 
+    public route: ActivatedRoute, 
     public loaderService: LoaderService, 
     private toastrSvc: ToastrService) {}
 
@@ -54,18 +56,23 @@ export class ReclamoInternoComponent implements OnInit {
 
      this.api.envioComponentes('SI') //ENVIA AL BUSCADOR OTRO STRING PARA HABILITARLO
 
-     //EL IF ESTE ES IMPORTANTISIMO, SINO BUSCA EL ULTIMO RECLAMO EN LA LISTA
-     if (numReclamo != ''){
-  
-    let listarReclamo = this.api.listarReclamoInd(numReclamo)
-    let talleres = this.api.talleres()
-    let tipoSolucion = this.api.opciones()
+     let listarReclamo = this.api.listarReclamoInd(numReclamo)
+     let talleres = this.api.talleres()
+     let tipoSolucion = this.api.opciones()
 
+    //EL IF ESTE ES IMPORTANTISIMO, SINO BUSCA EL ULTIMO RECLAMO EN LA LISTA
+    if (numReclamo != ''){
     forkJoin([listarReclamo, talleres, tipoSolucion])
     .subscribe( results => {
       this.datosReclamo = results[0]
       this.talleres = results[1]
       this.opciones = results[2]
+
+      this.talleres.forEach((taller: any) => {
+        if (taller.desactivado != true) {
+          this.talleresActivos.push(taller)
+        }
+      });
 
         //SI EL RECLAMO EXISTE, PERMITE APARECER A LA DESCRIPCION DEL PRODUCTO
         this.api.cargarProducto(this.datosReclamo[0].prodCodBar).subscribe((data)=>{
@@ -75,14 +82,19 @@ export class ReclamoInternoComponent implements OnInit {
         if (this.datosReclamo.pagado == 'Sí') {
           this.pagadoTaller = 1
         }
+
+        let opcion = this.opciones.find((x: any) => x?.estado == this.datosReclamo[0].estado)
+
+        let taller = this.talleres.find((x: any) => x?.taller == this.datosReclamo[0].taller)
+
         this.profileForm.patchValue({
           codigoBarras: this.datosReclamo[0].prodCodBar,
           seccion: this.datosReclamo[0].seccion,
           fecha: dayjs(this.datosReclamo[0].fecha).format('DD/MM/YYYY'),
-          estado: this.datosReclamo[0].estado,
+          estado: opcion.id.toString(), //OPCIONES LAS LEE COMO STRING
           motivo: this.datosReclamo[0].motivo,
           observaciones: this.datosReclamo[0].solucion,
-          taller: this.datosReclamo[0].taller,
+          taller: taller.id.toString(), //OPCIONES LAS LEE COMO STRING
           importe: this.datosReclamo[0].costo,
           pagado: this.pagadoTaller,
           cerrado: this.datosReclamo[0].cerrado,
@@ -90,6 +102,18 @@ export class ReclamoInternoComponent implements OnInit {
           reclamo: this.datosReclamo[0].reclamo
         })
     })
+    } else {
+      forkJoin([talleres, tipoSolucion])
+      .subscribe( results => {
+        this.talleres = results[0]
+        this.opciones = results[1]
+  
+        this.talleres.forEach((taller: any) => {
+          if (taller.desactivado != true) {
+            this.talleresActivos.push(taller)
+          }
+        });
+      })
     }
   }
 
@@ -104,45 +128,17 @@ export class ReclamoInternoComponent implements OnInit {
   //BOTON PARA GRABAR
   onSubmit() {
     let reclamo = {
-      Id: 126699,
-      //IdTipo: 1, 
-      //FechaCompra: "2023-08-05",
-      IdCliente: 99697, 
-      //ProdMarca: "",
-      //ProdPrecio: 999, 
-      //ProdFuv: "2023-08-05",
-      //PrometidoDia: "2023-08-10",
-      //IdTurno: 1, 
-      //Importe: 100, 
-      IdSolTipo: 1, 
-      //Ncred: "",
-      //FechaSol: "2023-08-07",
-      //Observaciones: "Prueba",
-      //IdOrigen: 1, 
-      //IdBc: 1, 
-      IdEmp: "T",
-      //Cantidad: 1,
-      //Ticket: false,
-      //NroTransferInt: "",
-      //IdOtr: 1, 
-      //IdTipoMmv: 1, 
-      //IdEmpleado: 1, 
-      //Usado: false,
-      //NroHojaLr: "789",
+      ProdCodBar: this.profileForm.value.codigoBarras,
+      ProdDescripcion: this.producto,
+      IdSec: this.profileForm.value.seccion, //HAY QUE PARSEARLO parseInt PARA VOLVERLO NUMBER Y COMPARARLO CON LA API DE SECCIONES 
+      Fecha: this.profileForm.value.fecha,
+      IdSolEstado: parseInt(this.profileForm.value.estado),
+      Motivo: this.profileForm.value.motivo,
+      Solucion: this.profileForm.value.observaciones,
+      IdTaller: parseInt(this.profileForm.value.taller),
+      Costo: this.profileForm.value.importe,
+      Pagado: this.profileForm.value.pagado, 
     }
-    // let reclamo = {
-    //   Id: this.datosReclamo?.id,
-    //   ProdCodBar: this.profileForm.value.codigoBarras,
-    //   ProdDescripcion: this.producto,
-    //   IdSec: this.profileForm.value.seccion, 
-    //   Fecha: this.profileForm.value.fecha,
-    //   IdSolEstado: this.profileForm.value.estado,
-    //   Motivo: this.profileForm.value.motivo,
-    //   Solucion: this.profileForm.value.observaciones,
-    //   IdTaller: this.profileForm.value.taller,
-    //   Costo: this.profileForm.value.importe || 0,
-    //   Pagado: this.profileForm.value.pagado || false
-    // }
     console.log(reclamo)
     if(this.datosReclamo == undefined){
       this.toastrSvc.success('Nuevo reclamo creado con éxito')
